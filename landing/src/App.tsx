6010cc3e8bd077713527d075a4a89ce22519b09d
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-04-03 16:50:18 -0500

    adding landing page stuff

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,13 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { LandingPage } from "./pages/landing";
+import { Navigation } from "./pages/navigation";
 import "./styling/App.css";
 import { theme } from "./theme/theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">hi</header>
-      </div>
+      <Router>
+        <div>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 }
---
+++ landing/src/pages/faq.tsx
@@ -0,0 +1,29 @@
+import { ThemedPage } from "../components/themedPage"; // Adjust the import path as necessary
+
+export function FAQ() {
+  return (
+    <ThemedPage>
+      <header>
+        <h1>FREQUENTLY ASKED QUESTIONS</h1>
+      </header>
+      <main>
+        <p>
+          Do we play every Friday and Saturday? We play every Friday and
+          Saturday if weather permits. Do we play in winter? We host outdoor
+          soccer events in the winter! We are also growing and are actively
+          looking for indoor venues. Do I need to bring cleats? We primarily
+          play on turf where cleats are welcomed. We sometimes play on grass and
+          occasionally play on hard courts, so please be sure to read the
+          description of each event Do you host events? We are organize soccer
+          events and rent out our services! Contact us to organize and host you
+          company hand non profit events. Where can I mainly find you? We have a
+          special focus in west and south side neighborhoods, where we live.
+          However we also host in then Northside. We are interested in hosting
+          in every neighborhood in Chicago, let us know if you think you have a
+          venue in your neighborhood where we could run pick ups! Are the events
+          free? Some. Please check the event description on Meetup.
+        </p>
+      </main>
+    </ThemedPage>
+  );
+}
