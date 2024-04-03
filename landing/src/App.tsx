977c1b360e0e77997cad1864cb5273f558d6b834
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-04-03 14:37:29 -0500

    Things compiling hopefully

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,26 +1,12 @@
-import { theme } from "../../common/theme/appTheme"
 import { ThemeProvider } from "styled-components";
-import "./App.css";
-import logo from "./logo.svg";
+import "./styling/App.css";
+import { theme } from "./theme/theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header">hi</header>
       </div>
     </ThemeProvider>
   );
