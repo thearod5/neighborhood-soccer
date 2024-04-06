5ace1819efc1ad3662ab00bb8467e0441873ea79
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-04-06 13:08:57 -0500

    Fixing the deployment

--- landing/src/index.tsx
+++ landing/src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import router from "./routes";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
