dc615da8b6e76b4b775855a9f749b2934ec20308
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-06-01 10:54:43 -0500

    Adding hamburger menu for navigation

---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,51 @@
+import CloseIcon from "@mui/icons-material/Close";
+import { Drawer, IconButton, MenuItem, Typography } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import NavData from "../content/nav.json";
+import "../styles/MobileMenu.css";
+
+interface MobileMenuProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const MobileMenu: React.FC<MobileMenuProps> = ({ open, onClose }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Drawer
+      anchor="right"
+      open={open}
+      onClose={onClose}
+      sx={{
+        width: "75%",
+      }}
+    >
+      <div className="mobile-menu">
+        <div className="mobile-menu-header">
+          <IconButton onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <div className="mobile-menu-content">
+          {NavData["pages"].map((link) => (
+            <MenuItem
+              key={link["link"]}
+              onClick={() => {
+                navigate(link["link"]);
+                onClose();
+              }}
+            >
+              <Typography variant="body1" style={{ fontWeight: "bold" }}>
+                {link["title"]}
+              </Typography>
+            </MenuItem>
+          ))}
+        </div>
+      </div>
+    </Drawer>
+  );
+};
+
+export default MobileMenu;
